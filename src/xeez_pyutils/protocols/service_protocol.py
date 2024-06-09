499e318b337f6cb5343bcf1d5268bcbf4a2b2896
Alexandre Bertuol <axdbertuol@gmail.com>
2024-06-09 17:25:52 -0300

    fix: remove src

--- src/xeez_pyutils/protocols/service_protocol.py
+++ src/xeez_pyutils/protocols/service_protocol.py
@@ -1,7 +1,7 @@
 from typing import Generic, List, Protocol, TypeVar
 
 from pydantic import BaseModel
-from src.xeez_pyutils.common import CommonQueryParams
+from xeez_pyutils.common import CommonQueryParams
 
 SchemaType = TypeVar("SchemaType", bound=BaseModel)
 
